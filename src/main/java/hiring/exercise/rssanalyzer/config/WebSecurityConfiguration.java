383873498cd3801f782a6ecda449e72466eaf48f
Camilo Pimienta <camilo040494@hotmail.com>
2019-06-18 08:19:10 -0500

    Security configuration for swagger and yml setup

---
+++ src/main/java/hiring/exercise/rssanalyzer/config/WebSecurityConfiguration.java
@@ -0,0 +1,22 @@
+package hiring.exercise.rssanalyzer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                                   "/configuration/ui",
+                                   "/swagger-resources",
+                                   "/configuration/security",
+                                   "/swagger-ui.html",
+                                   "/rss-analyzer/**",
+                                   "/swagger/ui/index",
+                                   "/webjars/**");
+    }
+
+}

5dd4ff9b3e92beb3bb7cabcf2a21c37b734d4b1f
Camilo Pimienta <camilo040494@hotmail.com>
2019-06-17 20:49:06 -0500

    Initial commit

---
+++ src/main/java/hiring/exercise/rssanalyzer/RssAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package hiring.exercise.rssanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hiring/exercise/rssanalyzer/RssAnalyzerApplicationTests.java
@@ -0,0 +1,16 @@
+package hiring.exercise.rssanalyzer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RssAnalyzerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

1c861a04095886fffc6e59277813ab61dac374c0
Camilo Pimienta <camilo040494@hotmail.com>
2019-06-18 08:17:11 -0500

    Model to be saved

---
+++ src/main/java/hiring/exercise/rssanalyzer/model/InfoRssData.java
@@ -0,0 +1,16 @@
+package hiring.exercise.rssanalyzer.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InfoRssData {
+  public int frecuency;
+  public String header;
+  public String link;
+
+  public InfoRssData() {
+  }
+  
+}
---
+++ src/main/java/hiring/exercise/rssanalyzer/model/MatchedRss.java
@@ -0,0 +1,13 @@
+package hiring.exercise.rssanalyzer.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MatchedRss {
+  
+  private int id;
+  private InfoRssData data;
+  
+}

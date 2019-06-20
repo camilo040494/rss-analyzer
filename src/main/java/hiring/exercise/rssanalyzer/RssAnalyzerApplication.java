181ed49b326e4325104e80caaef8803dda4aea57
Camilo Pimienta <camilo040494@hotmail.com>
2019-06-19 23:52:06 -0500

    Swagger on application for bugs

--- src/main/java/hiring/exercise/rssanalyzer/RssAnalyzerApplication.java
+++ src/main/java/hiring/exercise/rssanalyzer/RssAnalyzerApplication.java
@@ -3,7 +3,10 @@ package hiring.exercise.rssanalyzer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class RssAnalyzerApplication {
 
 	public static void main(String[] args) {
